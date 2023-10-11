b185dd771c01b5b72a83cad7829afafd03bc9495
Kata Durjyodhan Reddy <kdurjyodhan141@gmail.com>
2023-10-11 13:29:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,11 +31,19 @@ const lookup = {
   ",": ",",
 };
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
+function rot13(str) {
+  let output = ""; // Your Result goes here
   // Only change code below this line
+	str = str.toUpperCase();
+	for(let i = 0; i < str.length; i++){
+		if(lookup[str[i]]){
+			output+=lookup[str[i]];
+		}else{
+			output += str[i];
+		}
+	}
 
-  return; //return decodedArr
+  return output; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
